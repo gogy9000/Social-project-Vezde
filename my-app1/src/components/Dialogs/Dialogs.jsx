8275715cca5bbb58db96d63a8e38100748fd5719
gogy9000 <goropashnyj9000@gmail.com>
2021-01-28 23:35:38 +0500

    my-app1

--- my-app1/src/components/Dialogs/Dialogs.jsx
+++ my-app1/src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
        
-    <div>
-        Dialogs
+    <div className={s.Dialogs}>
+        <div className={s.Dialogs_items}>
+            <div className={s.Dialog+' '+s.active}>
+               Motherfucker1
+            </div>
+            <div className={s.Dialog}>
+               Motherfucker2
+            </div>
+            <div className={s.Dialog}>
+               Motherfucker3
+            </div>
+            <div className={s.Dialog}>
+               Motherfucker4
+            </div>
+            <div className={s.Dialog}>
+               Motherfucker5
+            </div>
+            <div className={s.Dialog}>
+               Motherfucker6
+            </div>
+        </div>
+        <div className={s.Messages}>
+            <div className={s.Message}>
+              fuck you
+            </div> 
+            <div className={s.Message}>   
+            no, fuck you
+            </div>
+            <div className={s.Message}>
+              where is my many, Lebovsky?
+            </div>
+            <div className={s.Message}>
+               what you say, motherfucker?
+            </div>
+            <div className={s.Message}>
+               yo yo yo
+            </div>
+            <div className={s.Message}>
+              no no no 
+            </div>
+        </div>    
+             
+        
     </div>
         
     )
--- my-app1/src/components/Header/Header.jsx
+++ my-app1/src/components/Header/Header.jsx
@@ -4,7 +4,10 @@ import s from './Header.module.css';
 const Header = () => {
     return (
         <header className={s.header}>
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQN1aHcEjTCfF7jhxIcJB1jyL_4BYEvoZRdQ&usqp=CAU' />
+           <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQN1aHcEjTCfF7jhxIcJB1jyL_4BYEvoZRdQ&usqp=CAU' />
+           <div className={s.logo}>
+              <h1>Vpizde</h1>
+           </div>
         </header>
     );
 }
