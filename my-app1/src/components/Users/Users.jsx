65d11cef565910c730e8b5546f7dcbd3e37ff0c2
gogy9000 <goropashnyj9000@gmail.com>
2021-04-03 12:33:23 +0500

    51 server API

--- my-app1/src/components/Users/Users.jsx
+++ my-app1/src/components/Users/Users.jsx
@@ -1,60 +1,22 @@
+import * as axios from 'axios'
 import React from 'react'
 import styles from './Users.module.css'
-
+import userPhoto from './../../assets/images/User.png'
 let Users = (props) => {
     if (props.users.length === 0 ) {
-        props.setUsers(
-             [
-                {
-                    id:1,
-                     photoURL:'http://archilab.online/images/1/123.jpg',
-                      followed: false,  fullName: ' Motherfucker!',
-                       status:'fuck you are!',
-                        location: {city:'Minsk', country: 'belarus'}
-                    },
-                {
-                    id:2,
-                     photoURL:'https://i.imgur.com/9NAj0jq.jpg', 
-                     followed: true,  fullName: ' Motherfucker!',
-                      status:'fuck you are!', location: {city:'Minsk',
-                       country: 'belarus'}
-                    },
-                {
-                    id:3,
-                     photoURL:'http://cs622426.vk.me/v622426834/409d/baLqspYwi84.jpg',
-                      followed: false,
-                        fullName: ' Motherfucker!',
-                         status:'fuck you are!',
-                          location: {city:'Minsk', country: 'belarus'}
-                        },
-                {
-                    id:4,
-                     photoURL:'https://cs16planet.ru/steam-avatars/images/avatar2700.jpg',
-                      followed: true,
-                        fullName: ' Motherfucker!',
-                         status:'fuck you are!',
-                          location: {city:'Minsk', country: 'belarus'}
-                        },
-                {
-                    id:5,
-                     photoURL:'https://ic.pics.livejournal.com/moscowfix/44589829/1232478/1232478_original.jpg',
-                      followed: false,
-                        fullName: ' Motherfucker!',
-                         status:'fuck you are!',
-                          location: {city:'Minsk',
-                           country: 'belarus'}},
-        
-              ],
-        )
-    }
-    debugger
+
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(Response => {
+            
+            props.setUsers(Response.data.items) })
+        }
+    
     return <div>
         {
             props.users.map( u => 
             <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoURL} className={styles.userPhoto} />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto } className={styles.userPhoto} />
                     </div>
                     <div>
                         {u.followed 
@@ -69,12 +31,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                          <div>{u.status}</div>
                    </span>
                    <span>
-                         <div>{u.location.country} </div>
-                         <div>{u.location.city} </div>
+                         <div>{'u.location.country'} </div>
+                         <div>{'u.location.city'} </div>
                          
                     </span>
                 </span>
--- my-app1/src/components/Users/UsersContainer.jsx
+++ my-app1/src/components/Users/UsersContainer.jsx
@@ -11,7 +11,7 @@ let mapStateToProps = (state) => {
 }
 let mapDispatchToProps =(dispatch) =>
 
-{debugger
+{
     return{
         follow: (userId) => {
             dispatch(followAC(userId))
